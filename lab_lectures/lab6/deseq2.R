83dd6215fc6b87cd3722ac1d6b6aa2cb8c0c594a
Niederhuth <niederhu@msu.edu>
2022-10-11 12:35:34 -0400

    stuff

--- lab_lectures/lab6/deseq2.R
+++ lab_lectures/lab6/deseq2.R
@@ -30,14 +30,60 @@ dds$sizeFactor
 dds <- estimateDispersions(dds)
 #Plot out the dispersions
 plotDispEsts(dds)
+#Variance stabilizing transformation
+vsd <- vst(dds, blind=FALSE)
+rld <- rlog(dds, blind=FALSE)
+#Calculate distance between samples
+sampleDists <- dist(t(assay(vsd)))
+#Plot sample heatmap
+library(pheatmap)
+library(RColorBrewer)
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(colnames(vsd), vsd$type)
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+#PCA of the samples
+plotPCA(vsd, intgroup=c("genotype"))
 #Call DE genes using the Wald Test
 dds <- nbinomWaldTest(dds)
 #Extract results for a pair-wise contrast
 resultsTable <- as.data.frame(results(dds, contrast=c("genotype","WT","h3h3")))
+head(resultsTable)
 #Show how many genes padj < 0.05
 nrow(na.omit(resultsTable[resultsTable$padj < 0.05,]))
-#Genes with 2-fold change or log2FC = 1
-nrow(na.omit(resultsTable[resultsTable$padj < 0.05 & resultsTable$log2FoldChange >= 1,]))
-
-
+#Genes with 2-fold change
+nrow(na.omit(resultsTable[resultsTable$padj < 0.05 & resultsTable$log2FoldChange >= 1 | resultsTable$padj < 0.05 & resultsTable$log2FoldChange <= -1,]))
+#Volcano Plot
+plotMA(results(dds, contrast=c("genotype","WT","h3h3")), ylim=c(-2,2))
+#Function for making a table of results for two conditions from dds
+makeResultsTable <- function(x,compFactor,conditionA,conditionB,lfcThreshold=0,filter=FALSE){
+  require(DESeq2)
+  bml <- sapply(levels(dds@colData[,compFactor]),function(lvl) rowMeans(counts(dds,
+                                          normalized=TRUE)[,dds@colData[,compFactor] == lvl]))
+  bml <- as.data.frame(bml)
+  y <- results(x,contrast=c(compFactor,conditionA,conditionB),
+               lfcThreshold=lfcThreshold,independentFiltering=filter)
+  y <- data.frame(id=gsub(pattern = "gene:",replacement = "",row.names(y)),
+                  sampleA=c(conditionA),sampleB=c(conditionB),
+                  baseMeanA=bml[,conditionA],baseMeanB=bml[,conditionB],
+                  log2FC=y$log2FoldChange,pval=y$pvalue,padj=y$padj)
+  row.names(y) <- c(1:nrow(y))
+  return(y)
+}
+#Make new results table
+res <- makeResultsTable(dds,"genotype","WT","h3h3",lfcThreshold=0,filter=TRUE)
+head(res)
+#Create a column where 1 = meets cutoff, 0 = does not meet cutoff
+res$sig <- ifelse(res$padj < 0.05 & res$log2FC >= 1 | res$padj < 0.05 & res$log2FC <= -1, 1, 0)
+head(res)
+table(res$sig)
+#Plot counts for the PGAZAT gene
+plotCounts(dds, gene="AT2G41850", intgroup="genotype")
+#Return the data table
+plotCounts(dds, gene="AT2G41850", intgroup="genotype", returnData = TRUE)
+pgazat <- plotCounts(dds, gene="AT2G41850", intgroup="genotype", returnData = TRUE)
 
